c56fc699ac63bdfb288ff3a788eac41f0ee11e96
aluno <aluno@udicentro.local>
2025-04-10 20:45:18 -0300

    quarto commit atv

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { fetchDados } from './fetchXML.js';
 import { fetchDados2 } from './fetchFetch.js';
 
  
-const url = 'https://vinicius-rss.github.io/async-json/dados.json';
+const url = 'https://vinicius-rss.github.io/async-json/';
 
  
 fetchDados(url);   
